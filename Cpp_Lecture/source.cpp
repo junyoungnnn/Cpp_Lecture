639560f529ff6f6e08f37cda7e4d9656f00ba874
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-09-12 19:47:24 +0900

    (2023.09.12) 얕은 복사와 깊은 복사

--- Cpp_Lecture/source.cpp
+++ Cpp_Lecture/source.cpp
@@ -2,119 +2,96 @@
 
 using namespace std;
 
-#pragma region 클래스
-	// 사용자 정의 데이터 유형으로 속성과 함수가 포함되어
-	// 있으며, 클래스를 통해 객체를 생성하여 접근하고 사용할 수
-	// 있는 집합체입니다.
+#pragma region 복사 생성자
+	// 같은 객체를 복사하여 생성시킬 때 
+	// 호출되는 생성자입니다.
 
-class GameObject
+class Item
 {
-	// 접근 자종자
-	// 클래스 내부의 포함되어 있는 속성에 접근 범위를
-	// 제한하는 지정자입니다.
-
-	// public
-	// 클래스 내부와 자기가 상속하고 있는 클래스 그리고 클래스 외부에서
-	// 접근을 허용하는 지정자입니다.
-
-	// protected
-	// 클래스 내부와 자기가 상속하고 있는 클래스까지만,
-	// 접근을 허용하는 지정자입니다.
-
-	// 기본 접근 지정자 (private)
-	// 클레스 내부에서만 접근을 허용하는 지정자입니다.
+private:
+	int price;
+	int* size;
 
 public:
-	float x;
-
-	void SendMesssage()
+	Item()
 	{
-		cout << "메세지" << endl;
+		price = 100;
+		size = new int;
+		*size = 999;
 	}
-protected:
-	double y;
-	// 패딩때문에 double을 아래로 내리는것이 좋음.
-private:
-	float z;
+	// 기본 생성자
+	// public Item() {  }
 
-	
+	Item(const Item& item)
+	{
+		price = item.price;
+		// 깊은 복사
+		size = new int;
+		*size = 450;
+	}
+
+	~Item()
+	{
+		delete size;
+	}
+
+	void Show()
+	{
+		cout << "price: " << price << endl;
+		cout << "*ssize: " << *size << endl;
+	}
 };
 #pragma endregion
 
-#pragma region 생성자와 소멸자
-	// <생성자>
-	// 클래스의 인스턴스가 생성되는 시점에서 자동으로
-	// 호출되는 특수한 멤버 함수입니다.
-
-	class Monster
-	{
-	private:
-		int health;
-
-	public:
-		Monster()
-		{
-			cout << "Monster 생성" << endl;
-		}
-
-		// 생성자의 경우 객체가 생성될 때 단 한 번만
-		// 호출되며, 생성자는 반환형이 존재하지 않기 때문에
-		// 생성자가 호출되기 전에는 객체에 대한 메모리는 할당하지 않습니다.
-
-		// <소멸자>
-		// 객체가 소멸될 때 자동으로 실행되는 클래스의 멤버 함수입니다.
-		~Monster()
-		{
-			cout << "Monster 소멸" << endl;
-		}
-
-		// 소멸자는 객체가 메모리에소 해제될 떄 단 한 번만
-		// 호출되며, 소멸자에는 매개변수를 생성하여 사용할 수 없습니다.
-	};
-#pragma endregion
 
 
 int main()
 {
-#pragma region 클래스
+#pragma region 복사 생성자
 
-	//	GameObject gameObject1;
-	//	GameObject gameObject2;
-	//	
-	//	// 함수의 주소는 코드 영역에 저장되기 때문.
-	//	cout << sizeof(GameObject) << endl;
-	//	/*
-	//	CODE(함수의 주소)
-	//	DATA(.rodata)
-	//		(init data)
-	//	BSS
-	//	HEAP
-	//	STACK -> gameObject1
-	//		  -> gameObject2
-	//	*/
-	//	
-	//	// 함수의 매개변수에  자신의 주소가 들어감(this)
-	//	gameObject1.SendMesssage();
-	//	gameObject2.SendMesssage();
-	//	
-	//	gameObject1.x = 100;
-	//	cout << "gameObject1.x: " << gameObject1.x << endl;
+	  // Item item1;
+	  // item1.Show();
+	  // 
+	  // Item item2(item1);
+	  // item2.Show();
 #pragma endregion
 
-#pragma region 생성자와 소멸자
+#pragma region 얕은 복사
+	// 객체를 복사할 때 주소값을 복사하여 같은 메모리를
+	// 가르키는 복사입니다.
 
-	Monster monster;
+	//	int* ptr1 = new int;
+	//	int* ptr2 = ptr1;
+	//	
+	//	*ptr2 = 1000;
+	//	
+	//	cout << "ptr1의 값: " << *ptr1 << endl;
+	//	cout << "ptr2의 값: " << *ptr2 << endl;
+	//	
+	//	cout << "ptr1의 주소: " << ptr1 << endl;
+	//	cout << "ptr2의 주소: " << ptr2 << endl;
+	//	
+	//	delete ptr1;
+	// delete ptr2; ERROR
 
-	// 포인터는 그냥 주소를 저장할 수 있는 변수일 뿐임.
-	// Monster *ptr은 그냥 8byte 메모리를 만들겠다. 생성자 호출 안됨.
-	Monster* ptr = new Monster;
-
-	delete ptr;
-
-	// 상속은 코드의 재활용성
-	// 부모는 자식의 코드를 못쓰지만
-	// 자식은 부모의 코드를 사용가능.
+	// 얕은 복사는 객체가 메모리에서 해제될 때 생성자의
+	// 실행 순서와 반대로 소멸자가 실행됩니다.
+	
+	// 복사한 객체가 먼저 해제되므로 원래 있던 객체가
+	// 해제될 떄는 이미 해제된 메모리에 접근하게 됩니다.
 #pragma endregion
+
+#pragma region 깊은 복사
+	// 객체를 복사할 때, 참조 값이 아닌 인스턴스 자체를
+	// 새로 복사하여 서로 다른 메모리를 생성하는 복사입니다.
+	  Item item1;
+	  item1.Show();
+
+	  Item item2(item1);
+	  item2.Show();
+
+#pragma endregion
+
 
 	return 0;
 }
