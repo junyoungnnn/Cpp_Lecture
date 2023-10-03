b8de79d38e830b1563a4c68360ac291de34a33a7
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-10-03 22:32:07 +0900

    과제중...

--- Cpp_Lecture/source.cpp
+++ Cpp_Lecture/source.cpp
@@ -1,8 +1,20 @@
 ﻿#include <iostream>
 #include <vector>
-#include <stdlib.h>
-#include <time.h>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <windows.h>
 using namespace std;
+
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+// #define UP "↑"
+// #define LEFT "←"
+// #define RIGHT "→"
+// #define DOWN "↓"
 
 #pragma region 선형 컨테이너
 	// 데이터를 선형으로 저장하며, 특별한 제약이나
@@ -20,17 +32,7 @@ using namespace std;
 
 #pragma endregion
 
-int seed = 0;
 const char* point[4] = { "→","←" ,"↑" ,"↓" };
-
-int Random()
-{
-	srand( time( NULL ) );
-	seed = rand() % 4;
-
-	return seed;
-}
-int random = Random();
 
 int main()
 {
@@ -67,31 +69,67 @@ int main()
 
 #pragma region 리듬 게임
 	int round = 0;
+	int seed = 0;
+	srand( time( NULL ) );
 	
-
 	while ( round < 5 )
 	{
 		round++;
 		std::vector<const char*> vector;
-		std::vector<char> answer;
+		std::vector<const char*> answer;
 
 		cout << "Q: ";
 		for ( int i = 0; i < round; i++ )
 		{
 			
-			vector.push_back( point[random]);
+			seed = rand() % 4;
+			vector.push_back( point[seed]);
 			cout << vector[i] << " ";
-		}
-		cout << endl;
+			cout << endl;
 
+				while ( 1 ) {
+				char key = 0;
+				if ( _kbhit() )
+				{
+					if ( answer.size() == round )
+					{
+						for ( int i = 0; i < round; i++ )
+						{
+							if ( strcmp( vector[i], answer[i] ) != 0 )
+								return 0;
+						}
+						cout << endl;
+						break;
+					}
+					key = _getch();
+					if ( key == -32 )
+					{
+						key = _getch();
+					}
 
-		for ( int i = 0; i < round; i++ )
-		{
-			
+					switch ( key )
+					{
+					case LEFT:
+						cout << "←";
+						answer.push_back( point[1] );
+						break;
+					case RIGHT:
+						cout << "→";
+						answer.push_back( point[0] );
+						break;
+					case UP:
+						cout << "↑";
+						answer.push_back( point[2] );
+						break;
+					case DOWN:
+						cout << "↓";
+						answer.push_back( point[3] );
+						break;
+					}
+				}
+			}
 		}
 	}
-
-
 #pragma endregion
 
 	return 0;
