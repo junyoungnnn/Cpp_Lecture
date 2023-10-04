f2f7cbe8ff9443bc87a0ba3178c85919a4c7e904
Jun-Young Noh <119721103+junyoungnnn@users.noreply.github.com>
2023-10-04 19:47:58 +0900

    (2023.10.04) 컨테이너 어댑터

---
+++ Cpp_Lecture/(2023.10.02) 선형 컨테이너.h
@@ -0,0 +1,173 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <deque>
+#include <cstdlib>
+#include <ctime>
+#include <conio.h>
+#include <windows.h>
+using namespace std;
+
+#define UP 72
+#define LEFT 75
+#define RIGHT 77
+#define DOWN 80
+
+// #define UP "↑"
+// #define LEFT "←"
+// #define RIGHT "→"
+// #define DOWN "↓"
+
+#pragma region 선형 컨테이너
+	// 데이터를 선형으로 저장하며, 특별한 제약이나
+	// 규칙이 없는 일반적인 컨테이너 입니다.
+
+// 선형 컨테이너
+// std::vector
+// 동적 배열
+// 런타임중 배열의 크기를 변경가능함.
+// 자료의 크기가 늘어나면 배열의 크기를 증가시키는게 아니라
+// 새로운 배열을 할당한다음 복사함.
+// C#에서는 list라고 부름.
+// std::deque
+// std::list (연관 컨테이너)
+
+#pragma endregion
+
+void GotoXY( int x, int y )
+{
+	// x, y 좌표 설정
+	COORD position = { x, y };
+
+	// 커서 이동
+	SetConsoleCursorPosition( GetStdHandle( STD_OUTPUT_HANDLE ), position );
+}
+
+const char* point[4] = { "→","←" ,"↑" ,"↓" };
+
+int main()
+{
+#pragma region Vector 컨테이너
+
+	//std::vector<int> vector;
+
+	//// push_back() : 뒤 쪽에 데이터를 저장하는 함수
+	//vector.push_back(10);
+	//vector.push_back(20);
+	//vector.push_back(30);
+
+	//cout << "vector.size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl; // 용량 두배증가. 컴퓨터마다 상의함(1.5~2배)
+
+	//for ( int i = 0;i < vector.size(); i++ )
+	//{
+	//	cout << vector[i] << endl;
+	//}
+
+	//vector.push_back( 40 );
+	//vector.push_back( 50 );
+	//vector.pop_back();
+
+	//cout << "vector.size: " << vector.size() << endl;
+	//cout << "capacity: " << vector.capacity() << endl;
+
+	//for ( int i = 0;i < vector.size(); i++ )
+	//{
+	//	cout << vector[i] << endl;
+	//}
+
+#pragma endregion
+
+#pragma region 리듬 게임
+	/*
+	int round = 1;
+	int seed = 0;
+	int hp = 5;
+	srand( time( NULL ) );
+
+	while ( round < 5 )
+	{
+		//round++;
+		std::vector<const char*> vector;
+		std::vector<const char*> answer;
+
+		cout << "Q: ";
+		for ( int i = 0; i < round; i++ )
+		{
+
+			seed = rand() % 4;
+			vector.push_back( point[seed] );
+			cout << vector[i] << " ";
+			cout << endl;
+
+				while ( 1 ) {
+				char key = 0;
+				if ( _kbhit() )
+				{
+					if ( answer.size() == round )
+					{
+						for ( int i = 0; i < round; i++ )
+						{
+							if ( strcmp( vector[i], answer[i] ) != 0 )
+								return 0;
+						}
+						cout << endl;
+						break;
+					}
+
+					key = _getch();
+					if ( key == -32 )
+					{
+						key = _getch();
+					}
+
+					switch ( key )
+					{
+					case LEFT:
+						cout << "←";
+						answer.push_back( point[1] );
+						break;
+					case RIGHT:
+						cout << "→";
+						answer.push_back( point[0] );
+						break;
+					case UP:
+						cout << "↑";
+						answer.push_back( point[2] );
+						break;
+					case DOWN:
+						cout << "↓";
+						answer.push_back( point[3] );
+						break;
+					}
+				}
+			}
+		}
+	}
+	*/
+#pragma endregion
+
+#pragma region deque 컨테이너
+	// Double Ended Queue
+	// 연속적인 메모리 공간이 아니라 두 메모리 공간을 연결 시켜 둔것.
+	// 백터에서 할 수 없는 것을 데큐로 한다.
+	// 프론트 팝
+
+	/*deque<int> deque;
+	deque.push_back( 10 );
+	deque.push_back( 20 );
+	deque.push_front( 99 );
+
+	for ( int i = 0; i < deque.size();i++ )
+	{
+		cout << deque[i] << endl;
+	}
+	deque.pop_front();
+	for ( int i = 0; i < deque.size();i++ )
+	{
+		cout << deque[i] << endl;
+	}*/
+#pragma endregion
+
+	return 0;
+}
--- Cpp_Lecture/source.cpp
+++ Cpp_Lecture/source.cpp
@@ -1,135 +1,61 @@
 ﻿#include <iostream>
-#include <vector>
-#include <cstdlib>
-#include <ctime>
-#include <conio.h>
-#include <windows.h>
+#include <stack>
+#include <queue>
 using namespace std;
 
-#define UP 72
-#define LEFT 75
-#define RIGHT 77
-#define DOWN 80
+#pragma region 컨테이너 어댑터
+// 기존 컨테이너의 인터페이스를 제한하여
+// 만든 기능이 제한되거나 변형된 컨테이너입니다.
 
-// #define UP "↑"
-// #define LEFT "←"
-// #define RIGHT "→"
-// #define DOWN "↓"
-
-#pragma region 선형 컨테이너
-	// 데이터를 선형으로 저장하며, 특별한 제약이나
-	// 규칙이 없는 일반적인 컨테이너 입니다.
-	
-// 선형 컨테이너
-// std::vector
-// 동적 배열
-// 런타임중 배열의 크기를 변경가능함.
-// 자료의 크기가 늘어나면 배열의 크기를 증가시키는게 아니라
-// 새로운 배열을 할당한다음 복사함.
-// C#에서는 list라고 부름.
-// std::deque
-// std::list (연관 컨테이너)
+// STACK은 LIFO 구조
+// QUEUE는 FIFO 구조
+// 이 둘은 백터 구조
+// 
+// PRIORITY QUEUE
 
 #pragma endregion
-
-const char* point[4] = { "→","←" ,"↑" ,"↓" };
 
 int main()
 {
-#pragma region Vector 컨테이너
+#pragma region 컨테이너 어댑터 (Stack)
 
-	//std::vector<int> vector;
+	//stack<int> stack;
 
-	//// push_back() : 뒤 쪽에 데이터를 저장하는 함수
-	//vector.push_back(10);
-	//vector.push_back(20);
-	//vector.push_back(30);
+	//stack.push( 10 );
+	//stack.push( 20 );
+	//stack.push( 30 );
+	//stack.push( 40 );
+	//stack.push( 50 );
 
-	//cout << "vector.size: " << vector.size() << endl;
-	//cout << "capacity: " << vector.capacity() << endl; // 용량 두배증가. 컴퓨터마다 상의함(1.5~2배)
+	//// 50
+	//// 40
+	//// 30
+	//// 20
+	//// 10
 
-	//for ( int i = 0;i < vector.size(); i++ )
-	//{
-	//	cout << vector[i] << endl;
+	//while(stack.size() > 0 )
+	//{//(stack.empty == false)
+	//	cout << "Stack의 Top : " << stack.top() << endl;
+	//	stack.pop();
 	//}
 
-	//vector.push_back( 40 );
-	//vector.push_back( 50 );
-	//vector.pop_back();
-
-	//cout << "vector.size: " << vector.size() << endl;
-	//cout << "capacity: " << vector.capacity() << endl;
-
-	//for ( int i = 0;i < vector.size(); i++ )
-	//{
-	//	cout << vector[i] << endl;
-	//}
-	
 #pragma endregion
 
-#pragma region 리듬 게임
-	int round = 0;
-	int seed = 0;
-	srand( time( NULL ) );
-	
-	while ( round < 5 )
+#pragma region  컨테이너 어댑터 (Queue)
+	queue<int> queue;
+
+	queue.push( 10 );
+	queue.push( 20 );
+	queue.push( 30 );
+	queue.push( 40 );
+	queue.push( 50 );
+
+	while ( queue.empty() == false )
 	{
-		round++;
-		std::vector<const char*> vector;
-		std::vector<const char*> answer;
-
-		cout << "Q: ";
-		for ( int i = 0; i < round; i++ )
-		{
-			
-			seed = rand() % 4;
-			vector.push_back( point[seed]);
-			cout << vector[i] << " ";
-			cout << endl;
-
-				while ( 1 ) {
-				char key = 0;
-				if ( _kbhit() )
-				{
-					if ( answer.size() == round )
-					{
-						for ( int i = 0; i < round; i++ )
-						{
-							if ( strcmp( vector[i], answer[i] ) != 0 )
-								return 0;
-						}
-						cout << endl;
-						break;
-					}
-					key = _getch();
-					if ( key == -32 )
-					{
-						key = _getch();
-					}
-
-					switch ( key )
-					{
-					case LEFT:
-						cout << "←";
-						answer.push_back( point[1] );
-						break;
-					case RIGHT:
-						cout << "→";
-						answer.push_back( point[0] );
-						break;
-					case UP:
-						cout << "↑";
-						answer.push_back( point[2] );
-						break;
-					case DOWN:
-						cout << "↓";
-						answer.push_back( point[3] );
-						break;
-					}
-				}
-			}
-		}
+		cout << queue.front() << endl;
+		queue.pop();
 	}
+
 #pragma endregion
 
 	return 0;
